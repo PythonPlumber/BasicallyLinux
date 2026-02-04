5dea49af97aabb6f5e81073a4ebe916f5882d648
Python Plumber <itzsenu2@gmail.com>
2026-02-04 23:59:59 +0530

    fix

--- src/boot.asm
+++ src/boot.asm
@@ -1,26 +1,24 @@
-[bits 32]
-
 section .multiboot
 align 4
-dd 0x1BADB002
-dd 0x00000000
-dd 0xE4524FFE
+    dd 0x1BADB002             ; Magic number
+    dd 0x00000003             ; Flags (align modules + mem info)
+    dd -(0x1BADB002 + 0x00000003) ; Checksum
 
 section .text
 global _start
 extern kernel_main
 
 _start:
-    cli
+    ; Setup stack and call kernel_main
     mov esp, stack_top
-    push ebx
-    push eax
+    push ebx                  ; Pointer to Multiboot info
     call kernel_main
-    add esp, 8
-    jmp $
+    cli
+.hang: hlt
+    jmp .hang
 
 section .bss
 align 16
 stack_bottom:
-    resb 16384
+    resb 16384 ; 16 KB stack
 stack_top:
